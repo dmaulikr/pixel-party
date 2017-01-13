8f8b1e5cdb0028a01abf77ea57bc7220efd95720
Ryan Laughlin <rofreg@gmail.com>
2017-01-12 21:44:29 -0500

    Handle pixelparty:// URI scheme

--- Pixel Party/AppDelegate.swift
+++ Pixel Party/AppDelegate.swift
@@ -47,6 +47,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             newWindow.makeKeyAndVisible()
         }
     }
+    
+    // Handle URL schemes
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        return true
+    }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
